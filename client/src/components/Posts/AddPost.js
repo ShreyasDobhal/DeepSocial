e23e89a437cb1f9435d97f061b2d6ba6a4e49a08
ShreyasDobhal <shreyasdobhal@gmail.com>
2020-07-22 22:01:02 +0530

    Added Image preview to Add-Post Component

--- client/src/components/Posts/AddPost.js
+++ client/src/components/Posts/AddPost.js
@@ -2,7 +2,29 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
 class AddPost extends Component {
+    
+    state = {
+        file:null
+    }
+
+    onChangeHandler = (event)=> {
+        console.log(event.target.files[0]);
+        this.setState({
+            file: URL.createObjectURL(event.target.files[0])
+        });
+    }
+
     render() {
+
+        let imagePreview = null;
+
+        if (this.state.file) {
+            imagePreview = (
+                <div className='add-post-preview-holder'>
+                    <img src={this.state.file} className='add-post-image-preview'/>
+                </div>
+            );
+        }
         return (
             <div className='add-post-container'>
                 <div className='add-post-elements-container'>
@@ -12,11 +34,13 @@ class AddPost extends Component {
                     <div className='add-post-input-holder'>
                         <textarea className='add-post-input' placeholder='Enter something to post'></textarea>
                         <div className='add-post-float-holder'>
-                            <i className="fa fa-picture-o add-post-send add-post-float-btn" aria-hidden="true"></i>
+                            <label for='imageUpload'><i className="fa fa-picture-o add-post-send add-post-float-btn" aria-hidden="true"></i></label>
                             <i className="fa fa-paper-plane add-post-send add-post-float-btn" aria-hidden="true"></i>
                         </div>
                     </div>
                 </div>
+                <input id='imageUpload' className='add-post-image-upload' type='file' onChange={this.onChangeHandler}/>
+                {imagePreview}
             </div>
         );
     }
